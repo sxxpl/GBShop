1366c1c229a958521c66a519f61101e418e05d10
Artem <artem-tikhonov-90@list.ru>
2022-12-28 20:00:51 +0300

    first commit

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Артем Тихонов on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
